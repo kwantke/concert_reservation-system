75cfa6236ebaec691ff54f7a909e0e2dd9663204
kokk <kokk7310@gmail.com>
2025-11-11 01:35:25 +0900

    feat: `JPA Coverter` 추가하여 enum 과 DB 간의 자동 변환 적용 및 변환시 에러 코드 추가 - `@Coverter`을 활용하여 enum -> DB, DB-> enum으로 자동 변환 적용 - enum과 DB간의 자동 변환시 발생할 수 있는 에러 코드 추가 - `Outbox` 생성하여 예매 후 발행(알림) 이벤트 관리을 위한 엔티티 추가

--- domain/src/main/java/com/kokk/domain/model/enums/ReservationStatus.java
+++ domain/src/main/java/com/kokk/domain/model/enums/ReservationStatus.java
@@ -1,16 +1,31 @@
 package com.kokk.domain.model.enums;
 
+import com.kokk.domain.model.exception.CoreException;
+import com.kokk.domain.model.exception.concert.ConcertErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @Getter
 public enum ReservationStatus {
   TEMPORARY_RESERVED("임시 예약",1),
-  CONFIRMED("확정 예약",2);
-
+  CONFIRMED("예약 확정",2),
+  ;
   private final String statusName;
   private final int statusCode;
 
+  private static Map<Integer, ReservationStatus> MAP =
+          Stream.of(values()).collect(Collectors.toMap(ReservationStatus::getStatusCode, outboxStatus -> outboxStatus));
 
+  public static ReservationStatus fromDbValue(int dbValue) {
+    ReservationStatus reservationStatus = MAP.get(dbValue);
+    if (reservationStatus == null) {
+      throw new CoreException(ConcertErrorCode.INVALID_RESERVATION_STATUS);
+    }
+    return reservationStatus;
+  }
 }
--- domain/src/main/java/com/kokk/domain/model/exception/concert/ConcertErrorCode.java
+++ domain/src/main/java/com/kokk/domain/model/exception/concert/ConcertErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ConcertErrorCode implements ErrorCode {
+  // 예매 관련 에러 코드
   UNAVAILABLE_CONCERT_SESSION_SEAT("예약 가능한 좌석이 아닙니다.", HttpStatus.BAD_REQUEST),
-
+  INVALID_OUTBOX_STATUS("아웃박스 상태는 필수 값입니다.", HttpStatus.BAD_REQUEST),
+  INVALID_RESERVATION_STATUS("예약 상태는 필수 값입니다.", HttpStatus.BAD_REQUEST)
   ;
   private final String message;
   private final HttpStatus httpStatus;
